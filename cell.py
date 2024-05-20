e6ca390bb4ee95b127cbc9d6ef9c682f71ac156a
GoldSonBR <gugadantas2010@gmail.com>
2024-05-20 14:05:40 -0300

    colocando atributos de x&y

--- cell.py
+++ cell.py
@@ -1,16 +1,20 @@
 from tkinter import Button
 class Cell:
-    def __init__(self, is_mine=False):
+    def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
 
     def create_btn_object(self, location):
         btn = Button(
             location,
-            text='Text'
+            width=12,
+            height=4,
+            text=f"{self.x}, {self.y}"
         )
-        btn.bind('<Button-1>',self.left_click_action)
-        btn.bind('<Button-3>', self.right_click_action)
+        btn.bind('<Button-1>', self.left_click_action)# LeftClick
+        btn.bind('<Button-3>', self.right_click_action)# RightCLick
         self.cell_btn_object = btn
 
     def left_click_action(self, event):
