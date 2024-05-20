a8ad951641555592c1cd1c3c715a99f2c29fe977
GoldSonBR <gugadantas2010@gmail.com>
2024-05-20 13:42:01 -0300

    Trabalhando nas celulas "Cells" e criando rows e columns dentro do frame do jogo com botoes. + nova setting + novo arquivo cell.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utils
 
@@ -12,7 +13,7 @@ root.resizable(False, False)
 
 top_frame =Frame(
     root,
-    bg='white', #Mudar depois
+    bg='black',
     width= settings.WIDTH,
     height=utils.height_prct(25)
 )
@@ -20,7 +21,7 @@ top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg='cyan', #Mudar depois
+    bg='black',
     width=utils.width_prct(25),
     height=utils.height_prct(75)
 )
@@ -28,7 +29,7 @@ left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(
     root,
-    bg='blue', #Mudar depois
+    bg='black',
     width=utils.width_prct(75),
     height=utils.height_prct(75)
 )
@@ -37,6 +38,13 @@ center_frame.place(
     y=utils.height_prct(25)
 )
 
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
 
 # Rodar Janela
 root.mainloop()
