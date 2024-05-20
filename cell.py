6b3a9a50211d903a37451c464482e37049becdb4
GoldSonBR <gugadantas2010@gmail.com>
2024-05-20 13:52:08 -0300

    novo segmento de "event" com os botoes esquerdo e direito do mouse.

--- cell.py
+++ cell.py
@@ -9,4 +9,14 @@ class Cell:
             location,
             text='Text'
         )
+        btn.bind('<Button-1>',self.left_click_action)
+        btn.bind('<Button-3>', self.right_click_action)
         self.cell_btn_object = btn
+
+    def left_click_action(self, event):
+        print(event)
+        print('Shit this was a lefty!')
+
+    def right_click_action(self, event):
+        print(event)
+        print('Shit this was a righty!')
